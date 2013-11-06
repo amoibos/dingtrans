aeb23aa49424ecd7e3fe15ec3cb2508980fb306f
Daniel Oelschlegel <amoibos@gmail.com>
2013-11-06 18:18:07 +0100

    minor code cleanups reduced memory usage added bug hint and README informations added small disclaimer

---
+++ README.txt
@@ -0,0 +1,3 @@
+Trans uses the same dictionaries as ding
+A query starts a search in both directions.
+The program itself stands under new-bsdl, the dictionary database is under GPLv2+.
--- trans.py
+++ trans.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-from sys import version_info, stdin
+from sys import version_info, stdin, argv
 from platform import system
 from time import clock
 
-__author__="Daniel Oelschlegel"
-__version__="0.01"
-__license__="bsdl"
+__author__ = "Daniel Oelschlegel"
+__copyright__ = __author__
+__version__ = "0.01"
+__license__ = "bsdl"
+
+#FIXME: python 3 give a different dictionary size as python 2
 
 if version_info[0] >= 3:
    raw_input = input
+else:
+    from itertools import izip as zip, imap as map
    
 ENCODING = stdin.encoding if system() == "Windows" else "utf-8"
 
@@ -21,29 +26,25 @@ def trimmer(text):
 def read_dictionary(file_name):
     '''Read out the dictionary file and save into RAM'''
     dict = {}
-    with open(file_name) if version_info[0] < 3 else open(file_name, encoding="utf-8") as dictionary_file:
-        for line in dictionary_file.readlines():
-             #ignoring comments and invalid vocabulary pairs
-            if line.startswith("#") or line=="\n":
-                continue
-            
-            #cloud definition recognised with pipe symbol("|")
-            left_side, right_side = map(lambda x: x.split("|"), line.split("::"))
-            
-            #split for alternative identifiers separated by semicolon(";")
-            collector = []
-            for item in zip(left_side, right_side):
-                for side_left in map(trimmer, item[0].split(";")):
-                    for side_right in map(trimmer, item[1].split(";")):
-                        collector.append((side_left, side_right))
-
-            #save mapping in both search directions
-            for item in collector:
-                for translation_pair in ((item[0], item[1], ">"), (item[1], item[0], "<")):
-                    value = translation_pair[0].lower().__hash__()
-                    if value not in dict:
-                        dict[value] = []
-                    dict[value].append(translation_pair)
+    for line in open(file_name) if version_info[0] < 3 else open(file_name, encoding="utf-8"):
+        #ignoring comments and invalid vocabulary entries
+        if line.startswith("#") or line=="\n":
+            continue
+        
+        #cloud definition recognised with pipe symbol("|")
+        left_side, right_side = map(lambda x: x.split("|"), line.split("::"))
+        
+        #split for alternative identifiers separated by semicolon(";")
+        collector = []
+        for item in zip(left_side, right_side):
+           for side_left in map(trimmer, item[0].split(";")):
+                for side_right in map(trimmer, item[1].split(";")):
+                    #save mapping in both search directions
+                    for translation_pair in ((side_left, side_right, ">"), (side_right, side_left, "<")):
+                        value = translation_pair[0].lower().__hash__()
+                        if value not in dict:
+                            dict[value] = []
+                        dict[value].append(translation_pair)
     return dict
            
 def main(file_name):
@@ -62,7 +63,7 @@ def make_unicode_python2(value):
 def gui(dict, time_in_sec):
     print("dictionary with %d word created in %.2f seconds" % 
         (len(dict), time_in_sec))
-    print("for abort press [ctrl]+[c]")
+    print("for exit press [ctrl]+[c]")
     while True:
         search_word_lower = make_unicode_python2(raw_input(": ").strip().lower())
         try:
@@ -73,4 +74,5 @@ def gui(dict, time_in_sec):
             print("word not found")
 
 if __name__ == "__main__":
-    main("dictionaries/de-en.txt")
+    print(__file__[:-3], __version__, "by", __author__)
+    main("dictionaries/de-en.txt" if len(argv) < 2 else argv[1])
