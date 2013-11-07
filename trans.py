9ff7b719e0e61a6d62d9047a7a1983ee4aa67dc0
Daniel Oelschlegel <amoibos@gmail.com>
2013-11-07 18:27:17 +0100

    cleanup python2 and 3 functionality reduced memory usage and startup time default output in list form reworked some docstrings

--- trans.py
+++ trans.py
@@ -4,10 +4,6 @@ from __future__ import print_function
 from sys import version_info, stdin, argv
 from platform import system
 from time import clock
-try:
-    from thread import start_new_thread
-except ImportError:
-     from _thread import start_new_thread
      
 __author__ = "Daniel Oelschlegel"
 __copyright__ = __author__
@@ -15,12 +11,14 @@ __version__ = "0.01"
 __license__ = "bsdl"
 
 #FIXME: python 3 give a different dictionary size as python 2
-#FIXME: empty queries return broken translations because of empty field(?)
+#FIXME: empty queries return broken translations because of empty fields(?)
 
 if version_info[0] >= 3:
-   raw_input = input
+    raw_input = input
+    from _thread import start_new_thread
 else:
-    from itertools import izip as zip, imap as map
+   from thread import start_new_thread
+   from itertools import izip as zip, imap as map
    
 ENCODING = stdin.encoding if system() == "Windows" else "utf-8"
 
@@ -31,17 +29,16 @@ def trimmer(text):
     return text.split("[")[0].split("{")[0].strip()
 
 def read_dictionary(file_name, start_time):
-    '''Read out the dictionary file and save into RAM'''
+    '''Read out the dictionary file and save into global dictionary'''
     for line in open(file_name) if version_info[0] < 3 else open(file_name, encoding="utf-8"):
         #ignoring comments and invalid vocabulary entries
         if line.startswith("#") or line=="\n":
             continue
         
-        #cloud definition recognised with pipe symbol("|")
+        #cloud definition recognised with pipe symbol
         left_side, right_side = map(lambda x: x.split("|"), line.split("::"))
 
-        #split for alternative identifiers separated by semicolon(";")
-        collector = []
+        #split for alternative identifiers separated by semicolon
         for item in zip(left_side, right_side):
            for side_left in map(trimmer, item[0].split(";")):
                 for side_right in map(trimmer, item[1].split(";")):
@@ -49,36 +46,42 @@ def read_dictionary(file_name, start_time):
                     for translation_pair in ((side_left, side_right, ">"), (side_right, side_left, "<")):
                         value = translation_pair[0].lower()
                         if value not in dict:
-                            dict[value] = []
-                        dict[value].append(translation_pair)
+                            dict[value] = [translation_pair]
+                        else:
+                            dict[value].append(translation_pair[1:])
+                        
     print("\ndictionary with %d word created in %.2f seconds" % (len(dict), clock() - start_time))
            
-def main(file_name):
-    start_new_thread(read_dictionary, (file_name, clock()))
-    ##DEBUG
-    #read_dictionary(file_name, clock())
-    gui()
-
 def output(value):
+    '''Output encoding, a special thing under windows'''
     if isinstance(value, str) and system() == "Windows" and version_info[0] < 3:
         return value.decode("utf-8").encode(ENCODING)
     return value
  
 def make_unicode_python2(value):
+    '''Inputs for python 2 must convert to utf8'''
     #TODO: TEST REQUIRED UNDER LINUX
     return value.decode(ENCODING).encode("utf8") if version_info[0] < 3 else value
     
 def gui():
+    '''User interface with missing abort condition'''
     print("for exit press [ctrl]+[c]")
     while True:
         search_word_lower = make_unicode_python2(raw_input(": ").strip().lower())
-        try:
-            for item in dict[search_word_lower]:
-                if item[0].lower() == search_word_lower:
-                    print(output("%s %s %s"  % (item[0], item[2]*3, item[1])))
-        except KeyError:
-            print("word not found")
+        if search_word_lower in dict:
+            item = dict[search_word_lower][0]
+            print(item[0], ": %s%s" % (item[2], item[1]) , end="")
+            for item in dict[search_word_lower][1:]:
+                print(output(", %s%s")  % (item[1], item[0]), end="")
+            print()
+        else:
+            print("%s not found" % search_word_lower)
+
+def main(file_name):
+    start_new_thread(read_dictionary, (file_name, clock()))
+    ##read_dictionary(file_name, clock()) #DEBUG
+    gui()
 
 if __name__ == "__main__":
-    print(__file__[:-3], __version__, "by", __author__)
+    print(__file__.strip(".py"), __version__, "by", __author__)
     main("dictionaries/de-en.txt" if len(argv) < 2 else argv[1])
